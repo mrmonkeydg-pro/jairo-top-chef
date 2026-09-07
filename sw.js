34b397315c6d97bc7faefe415794ba43fcbf397a
mrmonkeydg-pro <mr.monkeydg@gmail.com>
2026-09-07 12:00:14 +0200

    Añadir soporte PWA

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='jairo-top-chef-v1';const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
